6d8872503d4d818f14671ed357664145c956bc54
Luiz Eduardo Oku <luizeduardooku@hotmail.com>
2023-02-22 03:11:08 -0300

    fix: font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,24 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
+import { Roboto } from "@next/font/google";
 
 import { wrapper } from "@/store";
 import GlobalStyle from "@/styles/global.css";
 import theme from "@/styles/theme";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <main className={roboto.className}>
+          <Component {...pageProps} />
+        </main>
       </ThemeProvider>
     </>
   );
