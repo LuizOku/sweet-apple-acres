f57c6a1451ea5b4666e679403bd0b21e33986aa4
Luiz Eduardo Oku <luizeduardooku@hotmail.com>
2023-02-24 10:12:32 -0300

    fix: fixed the background image

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 import { Container, Title, Subtitle } from "./styles";
@@ -5,6 +6,17 @@ import { Container, Title, Subtitle } from "./styles";
 const Hero = () => {
   return (
     <Container>
+      <Image
+        src="/Crates.png"
+        fill
+        alt="crates"
+        style={{
+          objectFit: "cover",
+          objectPosition: "center",
+          zIndex: 0,
+          backgroundAttachment: "fixed",
+        }}
+      />
       <section>
         <Title>Sweet Apple Acres</Title>
         <Subtitle>Here you can bulk order products from anywhere.</Subtitle>
--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import { colors } from "@/styles/theme";
 
 export const Container = styled.section`
+  position: relative;
   width: 100vw;
   height: 500px;
   background: black;
@@ -11,9 +12,6 @@ export const Container = styled.section`
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  background-image: url("https://i.imgur.com/wWojP1K.jpg");
-  background-size: cover;
-  background-position: center;
   background-attachment: fixed;
   padding: 100px 100px 0px 100px;
 `;
@@ -22,10 +20,14 @@ export const Title = styled.h1`
   color: ${() => colors.white};
   font-size: 50px;
   margin-top: 10px;
+  z-index: 10;
+  position: relative;
 `;
 
 export const Subtitle = styled.h3`
   color: ${() => colors.white};
   font-size: 20px;
   margin-top: 10px;
+  z-index: 10;
+  position: relative;
 `;
